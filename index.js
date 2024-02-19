ab2c0a4a027975ffb55107e0de8e1ee768e9d688
Rizvana12 <kmrizvana3@gmail.com>
2024-02-19 16:41:57 +0530

    Modal app

---
+++ index.js
@@ -0,0 +1,31 @@
+// let modal = document.querySelector('.modal');
+// let overlay = document.querySelector('.overlay');
+
+// const openModal = () => {
+//     console.log("Modal is Open");
+//     modal.classList.add("active");
+//     overlay.classList.add("overlayactive");
+// };
+
+// const closeModal = () => {
+//     console.log("Modal is Closed");
+//     modal.classList.remove("active");
+//     overlay.classList.remove("overlayactive");
+
+// };
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+//   Modal open function
+const openModal = () => {
+  console.log("Modal is Open");
+  modal.classList.add("active");
+  overlay.classList.add("overlayactive");
+};
+
+// Modal close function
+const closeModal = () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("overlayactive");
+};
